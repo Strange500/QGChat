88e05395b02430ccb071fe60ccb1eb1568c16901
Milan Theron <milan.theron.etu@univ-lille.fr>
2025-02-10 14:09:40 +0100

    bdd class TODO

---
+++ WEB-INF/src/main/java/fr/univ/lille/s4a021/model/bdd/Authent.java
@@ -0,0 +1,5 @@
+package fr.univ.lille.s4a021.model.bdd;
+
+public class Authent {
+    //Verification User
+}
---
+++ WEB-INF/src/main/java/fr/univ/lille/s4a021/model/bdd/Connect.java
@@ -0,0 +1,5 @@
+package fr.univ.lille.s4a021.model.bdd;
+
+public class Connect {
+    // Class to getConnection for DAO constructor
+}
