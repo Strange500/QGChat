2a715043eac8fd6f2b568cb807de2eb45c0a7d68
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-02-26 15:31:49 +0100

    Channel.java

---
+++ WEB-INF/src/main/java/fr/univ/lille/s4a021/servlet/Channel.java
@@ -0,0 +1,70 @@
+package fr.univ.lille.s4a021.servlet;
+
+import fr.univ.lille.s4a021.dao.ChannelDAO;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/channel")
+public class Channel extends jakarta.servlet.http.HttpServlet {
+
+    private static ChannelDAO channelDAO;
+
+    public void doGet(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse res)
+            throws jakarta.servlet.ServletException, java.io.IOException {
+
+        String action = req.getParameter("action");
+
+        if (action == null) {
+            res.sendRedirect("index.jsp");
+            return;
+        }
+
+        if (channelDAO == null) {
+            try {
+                channelDAO = new ChannelDAO();
+            } catch (java.sql.SQLException e) {
+                e.printStackTrace();
+                res.sendRedirect("error.jsp");
+                return;
+            }
+        }
+
+        switch (action) {
+            case "create":
+                String name = req.getParameter("name");
+                try {
+                    channelDAO.createChannel(name);
+                    res.sendRedirect("home.jsp");
+                } catch (java.sql.SQLException e) {
+                    e.printStackTrace();
+                    res.sendRedirect("error.jsp");
+                }
+                break;
+            case "delete":
+                int cid = Integer.parseInt(req.getParameter("cid"));
+                try {
+                    channelDAO.deleteChannel(cid);
+                    res.sendRedirect("home.jsp");
+                } catch (java.sql.SQLException e) {
+                    e.printStackTrace();
+                    res.sendRedirect("error.jsp");
+                }
+                break;
+            case "update":
+                int cid2 = Integer.parseInt(req.getParameter("cid"));
+                String newName = req.getParameter("newName");
+                try {
+                    channelDAO.updateChannel(cid2, newName);
+                    res.sendRedirect("home.jsp");
+                } catch (java.sql.SQLException e) {
+                    e.printStackTrace();
+                    res.sendRedirect("error.jsp");
+                }
+                break;
+            default:
+                res.sendRedirect("index.jsp");
+
+        }
+
+
+    }
+}
