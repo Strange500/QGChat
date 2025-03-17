a39b766473e2cbdb14e461833acece15eca51663
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-03-17 16:12:01 +0100

    fix api

--- WEB-INF/src/fr/univ/lille/s4a021/controller/ApiController.java
+++ WEB-INF/src/fr/univ/lille/s4a021/controller/ApiController.java
@@ -5,10 +5,12 @@ import fr.univ.lille.s4a021.dao.ChannelDAO;
 import fr.univ.lille.s4a021.dao.MessageDAO;
 import fr.univ.lille.s4a021.dto.Channel;
 import fr.univ.lille.s4a021.dto.Message;
+import fr.univ.lille.s4a021.dto.User;
 import fr.univ.lille.s4a021.exception.ConfigErrorException;
 import fr.univ.lille.s4a021.exception.MyDiscordException;
 import fr.univ.lille.s4a021.exception.dao.DataAccessException;
 import fr.univ.lille.s4a021.exception.dao.channel.ChannelNotFoundException;
+import fr.univ.lille.s4a021.exception.dao.user.UserNotFoundException;
 import fr.univ.lille.s4a021.model.bdd.Util;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,24 +19,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @WebServlet("/api/*")
 public class ApiController extends AbstractController {
-    private ChannelDAO channelDAO;
-    private MessageDAO messageDAO;
 
     @Override
-    public void init() throws ServletException {
-        super.init();
-        this.channelDAO = (ChannelDAO) getServletContext().getAttribute("channelDAO");
-        this.messageDAO = (MessageDAO) getServletContext().getAttribute("messageDAO");
+    protected void processAction(String action, HttpServletRequest req, HttpServletResponse res) {
+        return;
     }
 
     @Override
-    protected void processAction(String action, HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException, MyDiscordException {
+    protected void processNoAuthAction(String action, HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException, MyDiscordException {
         switch (req.getMethod()) {
             case "GET":
                 doGet(req, res);
@@ -64,19 +63,15 @@ public class ApiController extends AbstractController {
             res.sendError(HttpServletResponse.SC_BAD_REQUEST, "Erreur : Valeur de l'argument invalide. Attendu : 'channel' ou 'message'.");
         }
 
-        // Check session
-        HttpSession session = req.getSession(false);
-        try {
-            if (session == null || !Util.userIsConnected(session)) {
-                res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User not authenticated"); // à changer pour demander authentification
-                return;
-            }
-        } catch (ConfigErrorException e) {
-            throw new RuntimeException(e);
-        }
+
 
         // Get all message per channel
-        int userId = Util.getUid(session);
+        String token = req.getHeader("Authorization");
+        Integer userId = getUserIdFromToken(token);
+        if (userId == null) {
+            res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
         try {
             List<Channel> channels = channelDAO.getSubscribedChannels(userId);
             Map<Channel, List<Message>> channelMessages = new HashMap<>();
@@ -92,5 +87,23 @@ public class ApiController extends AbstractController {
         } catch (DataAccessException | ChannelNotFoundException e) {
             res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error retrieving channels or messages");
         }
+    }
+
+    private Integer getUserIdFromToken(String baseToken) {
+        try {
+            String rawToken = baseToken.split(" ")[1];
+            String token = new String(Base64.getDecoder().decode(rawToken));
+            String mail = token.split(":")[0];
+            String password = token.split(":")[1];
+
+            if (userDAO.authenticateUser(mail, password)) {
+               return userDAO.getUserByMail(mail).getUid();
+            }
+        } catch (DataAccessException e) {
+            return null;
+        } catch (UserNotFoundException e) {
+            throw null;
+        }
+        return null;
     }
 }
